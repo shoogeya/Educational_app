526dfef0e1de564f33ff843172d381f2a36054ab
shoogeya <47056307+shoogeya@users.noreply.github.com>
2025-10-16 19:02:11 +0300

    Update config.dart

--- lib/app/config/config.dart
+++ lib/app/config/config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class Config {
   static const bool liveMode = false;
-  static const String baseUrlTest = 'https://apis.masarplatform.com/api/';
+  static const String baseUrlTest = '';
   static const String baseUrlLive = '';
   static const String baseUrl = liveMode == true ? baseUrlLive : baseUrlTest;
 }
